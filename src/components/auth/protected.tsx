8a69398959b3f4bd1aebe0d4435bb812b3a4309b
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-10-10 19:21:35 +0530

    Implement Protected component

--- src/components/auth/protected.tsx
+++ src/components/auth/protected.tsx
@@ -1,3 +1,24 @@
-export default function Protected() {
-  return <div>Protected</div>;
+"use client";
+
+import { useEffect, ReactNode } from "react";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useRouter } from "next/navigation";
+
+export default function Protected({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  const { user, loading, fetchUser } = useAuthStore();
+
+  useEffect(() => {
+    if (!user) fetchUser();
+  }, []);
+
+  useEffect(() => {
+    if (!loading && user === null) {
+      router.push("/login");
+    }
+  }, [loading, user]);
+
+  if (loading) return <div>Loading...</div>;
+
+  return <>{children}</>;
 }
