890063ad29270d3b217f8288cf33092fda39f6fa
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-10-22 20:58:04 +0530

    Add notesStorage constant for Appwrite configuration

--- src/constans/appwrite.ts
+++ src/constans/appwrite.ts
@@ -1,5 +1,6 @@
 const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DB!;
 const classroomTable = process.env.NEXT_PUBLIC_APPWRITE_CLASSROOM_TABLE!;
 const usersTable = process.env.NEXT_PUBLIC_APPWRITE_USERS_TABLE!;
+const notesStorage = process.env.NEXT_PUBLIC_APPWRITE_NOTES_BUCKET!;
 
-export { databaseId, classroomTable, usersTable };
+export { databaseId, classroomTable, usersTable, notesStorage };
