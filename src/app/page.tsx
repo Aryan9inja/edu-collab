2c2c729ffbac4776e81d20f12f120dd93b38cbaf
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-10-23 22:34:14 +0530

    Refactor Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-"use client";
-import { authService } from "@/services/auth";
-import { toast } from "sonner";
+import StartingModal from "@/components/startingModal";
 
-export default function Home(){
-    const handleLogout = async () => {
-        try {
-            await authService.logoutUser();
-            toast.success("Logged out successfully");
-        } catch (error) {
-            toast.error("Error logging out");
-            console.error(error);
-            return;
-        }
-    };
-    return <button onClick={handleLogout}>Click me</button>;
+export default function Home() {
+  return <StartingModal />;
 }
